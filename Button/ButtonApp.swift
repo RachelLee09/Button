c5c77ebcb4b5f3b9fce86552c8c7d2e6c8379301
Rachel Lee <rachellee3309@hotmail.com>
2024-07-26 09:28:26 -0700

    Initial Commit

---
+++ Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
